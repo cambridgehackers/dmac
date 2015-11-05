99c39781c8887b1dd002912474dc1b620d05c298
Jamey Hicks <jamey.hicks@gmail.com>
2015-11-05 16:38:03 -0500

    interface names updated

--- src/generated/GeneratedTypes.h
+++ src/generated/GeneratedTypes.h
@@ -18,7 +18,31 @@ typedef struct TileControl {
     uint8_t tile : 2;
     TileState state;
 } TileControl;
-typedef enum IfcNames { NoInterface, IfcNames_MemServerRequestS2H, IfcNames_MMURequestS2H, IfcNames_MemServerIndicationH2S, IfcNames_MMUIndicationH2S, IfcNames_DmaIndicationH2S0, IfcNames_DmaIndicationH2S1, IfcNames_DmaRequestS2H0, IfcNames_DmaRequestS2H1,  } IfcNames;
+typedef enum IfcNames { 
+  NoInterface,
+  IfcNames_MemServerRequestS2H,
+  IfcNames_MMURequestS2H,
+  IfcNames_MemServerIndicationH2S,
+  IfcNames_MMUIndicationH2S,
+  IfcNames_DmaRequestS2H0,
+  IfcNames_DmaRequestS2H1,
+  IfcNames_DmaRequestS2H2,
+  IfcNames_DmaRequestS2H3,
+  IfcNames_DmaRequestS2H4,
+  IfcNames_DmaRequestS2H5,
+  IfcNames_DmaRequestS2H6,
+  IfcNames_DmaRequestS2H7,
+
+  IfcNames_DmaIndicationH2S0,
+  IfcNames_DmaIndicationH2S1,
+  IfcNames_DmaIndicationH2S2,
+  IfcNames_DmaIndicationH2S3,
+  IfcNames_DmaIndicationH2S4,
+  IfcNames_DmaIndicationH2S5,
+  IfcNames_DmaIndicationH2S6,
+  IfcNames_DmaIndicationH2S7
+
+} IfcNames;
 
 
 int MemServerRequest_addrTrans ( struct PortalInternal *p, const uint32_t sglId, const uint32_t offset );
--- src/generated/IfcNames.bsv
+++ src/generated/IfcNames.bsv
@@ -1,2 +1,27 @@
 
-typedef enum {NoInterface, IfcNames_MemServerRequestS2H,IfcNames_MMURequestS2H,IfcNames_MemServerIndicationH2S,IfcNames_MMUIndicationH2S,IfcNames_DmaIndicationH2S0,IfcNames_DmaIndicationH2S1,IfcNames_DmaRequestS2H0,IfcNames_DmaRequestS2H1} IfcNames deriving (Eq,Bits);
+typedef enum {
+   NoInterface,
+   IfcNames_MemServerRequestS2H,
+   IfcNames_MMURequestS2H,
+   IfcNames_MemServerIndicationH2S,
+   IfcNames_MMUIndicationH2S,
+
+   IfcNames_DmaRequestS2H0,
+   IfcNames_DmaRequestS2H1,
+   IfcNames_DmaRequestS2H2,
+   IfcNames_DmaRequestS2H3,
+   IfcNames_DmaRequestS2H4,
+   IfcNames_DmaRequestS2H5,
+   IfcNames_DmaRequestS2H6,
+   IfcNames_DmaRequestS2H7,
+
+   IfcNames_DmaIndicationH2S0,
+   IfcNames_DmaIndicationH2S1,
+   IfcNames_DmaIndicationH2S2,
+   IfcNames_DmaIndicationH2S3,
+   IfcNames_DmaIndicationH2S4,
+   IfcNames_DmaIndicationH2S5,
+   IfcNames_DmaIndicationH2S6,
+   IfcNames_DmaIndicationH2S7
+
+} IfcNames deriving (Eq,Bits);
