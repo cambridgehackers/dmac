db1e8f6aa98e9d07d95190858920e53416ce3537
Jamey Hicks <jamey.hicks@gmail.com>
2015-11-11 16:25:56 -0500

    use howto format for doc because it has fewer blank pages

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -230,7 +230,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'dmac.tex', u'DMA Controller Documentation',
-   u'Jamey Hicks', 'manual'),
+   u'Jamey Hicks', 'howto'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
